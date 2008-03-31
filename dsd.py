19749db4de7c089d8d9d7079cebe1376059a26c0
Ryan May <rmay31@gmail.com>
2008-03-31 22:17:34 +0000

    Finally get the scaling right for calculating the exponential slope parameter from LWC.
    git-svn-id: svn+ssh://bragg-tun/home/rmay/src_repos/py_pkgs/trunk@18 30c3dafe-ad74-4c27-8e40-3f69a20387b2

--- dsd.py
+++ dsd.py
@@ -1,12 +1,13 @@
 import numpy as N
 
-mp_N0 = 8.0e3
-density_water = 1.0e3
+mp_N0 = 8.0e3 # m^-3 mm^-1
+density_water = 1.0e3 # Kg m^-3
 
 def mp_slope_3rd(lwc):
     '''Gives the slope for the Marshall-Palmer drop size distribution
-       given the liquid water content mixing ratio in g/kg'''
-    return (N.pi * density_water * mp_N0 * 1.0e6 / lwc)**0.25
+       given the liquid water content mixing ratio in g/kg. Returns
+       slope in mm^-1.'''
+    return (N.pi * density_water * mp_N0 * 1.0e-6 / lwc)**0.25
 
 def mp_from_lwc(d, lwc):
     return marshall_palmer(d, mp_slope_3rd(lwc))
@@ -18,15 +19,16 @@ def exponential(d, lam, N0):
     return gamma(d, lam, N0, 0.0)
 
 def gamma(d, lam, N0, mu):
-    return (N0 * (d[N.newaxis,:]**mu) * N.exp(-lam * d)).squeeze()
+    return (N0 * d[:,N.newaxis]**mu * N.exp(-lam[N.newaxis,:]
+        * d[:,N.newaxis])).squeeze()
 
 if __name__ == '__main__':
     import pylab as P
     lwc = 19.0
-    d = N.linspace(0.01, 1.0, 100)
+    d = N.linspace(0.01, 10.0, 100)
     mp = mp_from_lwc(d, lwc)
-    P.semilogy(d, mp)
-    P.xlabel('Diameter (cm)')
-    P.ylabel('Concentration (#/m^3)')
-    P.title('Marshall-Palmer Distribution for %.2f g/kg LWC' % lwc)
+    P.semilogy(d, mp, 'b')
+    P.xlabel('Diameter (mm)')
+    P.ylabel(r'Concentration (# m$^{-3}$)')
+    P.title(r'Marshall-Palmer Distribution for %.1f g kg$^{-1}$ LWC' % lwc)
     P.show()
