63662db00672d7b83253bb8a3daa3aa7f0465f2a
Ryan May <rmay31@gmail.com>
2008-04-02 17:37:51 +0000

    Add file containing _frequenly_ used constants
    git-svn-id: svn+ssh://bragg-tun/home/rmay/src_repos/py_pkgs/trunk@23 30c3dafe-ad74-4c27-8e40-3f69a20387b2

---
+++ constants.py
@@ -0,0 +1,20 @@
+from math import pi
+c = 3.e8 # Speed of light in m/s
+free_space_imp = 120*pi # Impedence of free space
+k = 1.38e-23 # Boltzmann's constant in J/K
+
+#Mass conversions
+g_per_kg = 1000.
+
+#Distance conversions
+cm_per_meter = 100.
+mm_per_meter = 1000.
+nmi_per_meter = 1./1852.354944
+
+#Angular conversion
+deg_per_rad = 180./pi # Duh.
+
+#Time conversions
+sec_per_day = 86400. # seconds in one day = 60 * 60 * 24
+
+
