1fb87f4bd5c5e54432d0ad0604abfdd1745c2847
Ryan May <rmay31@gmail.com>
2008-04-02 17:56:03 +0000

    Convert to using only MKS.  This should fix any problems with wondering what units are used in my functions.  Plots can then manually convert quantities.
    git-svn-id: svn+ssh://bragg-tun/home/rmay/src_repos/py_pkgs/trunk@25 30c3dafe-ad74-4c27-8e40-3f69a20387b2

--- dsd.py
+++ dsd.py
@@ -1,13 +1,13 @@
 import numpy as N
+from constants import density_water, mm_per_m
 
-mp_N0 = 8.0e3 # m^-3 mm^-1
-density_water = 1.0e3 # Kg m^-3
+mp_N0 = 8.0e3 * mm_per_m # m^-3 mm^-1 * mm/m -> m^-4
 
 def mp_slope_3rd(lwc):
     '''Gives the slope for the Marshall-Palmer drop size distribution
-       given the liquid water content mixing ratio in g/kg. Returns
-       slope in mm^-1.'''
-    return (N.pi * density_water * mp_N0 * 1.0e-6 / lwc)**0.25
+       given the liquid water content density in kg/m^3. Returns
+       slope in m^-1.'''
+    return (N.pi * density_water * mp_N0 / lwc)**0.25
 
 def mp_from_lwc(d, lwc):
     return marshall_palmer(d, mp_slope_3rd(lwc))
@@ -22,12 +22,14 @@ def gamma(d, lam, N0, mu):
     return N0 * d**mu * N.exp(-lam * d)
 
 if __name__ == '__main__':
-    import pylab as P
-    lwc = 19.0
-    d = N.linspace(0.01, 10.0, 100)
+    import matplotlib.pyplot as P
+    from constants import g_per_kg
+    lwc = 19.0 / g_per_kg 
+    d = N.linspace(0.01, 10.0, 100) / mm_per_m # Up to 10mm in meters
     mp = mp_from_lwc(d, lwc)
-    P.semilogy(d, mp, 'b')
+    P.semilogy(d * mm_per_m, mp / mm_per_m, 'b')
     P.xlabel('Diameter (mm)')
-    P.ylabel(r'Concentration (# m$^{-3}$)')
-    P.title(r'Marshall-Palmer Distribution for %.1f g kg$^{-1}$ LWC' % lwc)
+    P.ylabel(r'Concentration (# m$^{-3}$ mm$^{-1}$)')
+    P.title(r'Marshall-Palmer Distribution for %.1f g kg$^{-1}$ LWC'
+        % (lwc * g_per_kg))
     P.show()
