837d0119dc06c5849389401e57412b3b1b642e34
Ryan May <rmay31@gmail.com>
2008-04-02 17:55:09 +0000

    Update meters->m.  Add some more constants and do some internal deriving/consistency
    git-svn-id: svn+ssh://bragg-tun/home/rmay/src_repos/py_pkgs/trunk@24 30c3dafe-ad74-4c27-8e40-3f69a20387b2

--- constants.py
+++ constants.py
@@ -1,20 +1,27 @@
 from math import pi
+
+#Physical constants
 c = 3.e8 # Speed of light in m/s
 free_space_imp = 120*pi # Impedence of free space
 k = 1.38e-23 # Boltzmann's constant in J/K
+density_water = 1e3 # Density of liquid water in kg m^-3
 
 #Mass conversions
 g_per_kg = 1000.
 
 #Distance conversions
-cm_per_meter = 100.
-mm_per_meter = 1000.
-nmi_per_meter = 1./1852.354944
+cm_per_m = 100.
+mm_per_m = 1000.
+nmi_per_m = 1./1852.354944
 
 #Angular conversion
 deg_per_rad = 180./pi # Duh.
 
 #Time conversions
-sec_per_day = 86400. # seconds in one day = 60 * 60 * 24
+sec_per_min = 60.
+min_per_hour = 60.
+sec_per_hour = sec_per_min * min_per_hour
+hour_per_day = 24.
+sec_per_day = sec_per_hour * hour_per_day
 
 
