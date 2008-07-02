50a3cc3aba49a306a51baee36a0f5e311896d72d
Ryan May <rmay31@gmail.com>
2008-07-02 02:33:16 +0000

    Add a raindrop fallspeed function.  Also, update to new common idioms for pyplot and numpy.
    git-svn-id: svn+ssh://bragg-tun/home/rmay/src_repos/py_pkgs/trunk@39 30c3dafe-ad74-4c27-8e40-3f69a20387b2

--- dsd.py
+++ dsd.py
@@ -1,4 +1,4 @@
-import numpy as N
+import numpy as np
 from constants import density_water, mm_per_m
 
 mp_N0 = 8.0e3 * mm_per_m # m^-3 mm^-1 * mm/m -> m^-4
@@ -7,7 +7,7 @@ def mp_slope_3rd(lwc):
     '''Gives the slope for the Marshall-Palmer drop size distribution
        given the liquid water content density in kg/m^3. Returns
        slope in m^-1.'''
-    return (N.pi * density_water * mp_N0 / lwc)**0.25
+    return (np.pi * density_water * mp_N0 / lwc)**0.25
 
 def mp_from_lwc(d, lwc):
     '''Returns the Marshall-Palmer distribution weights corresponding for the
@@ -31,17 +31,25 @@ def gamma(d, lam, N0, mu):
     '''Returns the modifed gamma distribution weights corresponding for the
        given diameters using the given slope, intercept, and shpe parameters.
        All quantities should be in MKS.'''
-    return N0 * d**mu * N.exp(-lam * d)
+    return N0 * d**mu * np.exp(-d * lam)
+
+def rain_fallspeed(d):
+    '''Returns the raindrop fallspeed in m s^-1 given the drop diameter in m'''
+    d = d * mm_per_m # Formulas need diameter in mm.  Assumed passed in as m.
+    #From Brandes et al. 2002
+    #vt = -0.1021 + 4.932*d - 0.9551*d**2 + 0.07934*d**3 - 0.002362*d**4
+    vt2 = -0.1021 + d*(4.932 + d*(-0.9551 + d*(0.07934 - 0.002362*d)))
+    return vt
 
 if __name__ == '__main__':
-    import matplotlib.pyplot as P
+    import matplotlib.pyplot as plt
     from constants import g_per_kg
     lwc = 19.0 / g_per_kg 
-    d = N.linspace(0.01, 10.0, 100) / mm_per_m # Up to 10mm in meters
+    d = np.linspace(0.01, 10.0, 100) / mm_per_m # Up to 10mm in meters
     mp = mp_from_lwc(d, lwc)
-    P.semilogy(d * mm_per_m, mp / mm_per_m, 'b')
-    P.xlabel('Diameter (mm)')
-    P.ylabel(r'Concentration (# m$^{-3}$ mm$^{-1}$)')
-    P.title(r'Marshall-Palmer Distribution for %.1f g kg$^{-1}$ LWC'
+    plt.semilogy(d * mm_per_m, mp / mm_per_m, 'b')
+    plt.xlabel('Diameter (mm)')
+    plt.ylabel(r'Concentration (# m$^{-3}$ mm$^{-1}$)')
+    plt.title(r'Marshall-Palmer Distribution for %.1f g kg$^{-1}$ LWC'
         % (lwc * g_per_kg))
-    P.show()
+    plt.show()
