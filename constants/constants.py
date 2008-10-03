597d22ac46060216d5d313529e1cc738cb155e36
Ryan May <rmay31@gmail.com>
2008-10-03 21:05:01 +0000

    Remove constants that are duplicated in scipy.constants.  Add many more meteorologically significant constants.
    git-svn-id: svn+ssh://bragg-tun/home/rmay/src_repos/py_pkgs/trunk@48 30c3dafe-ad74-4c27-8e40-3f69a20387b2

--- constants/constants.py
+++ constants/constants.py
@@ -1,32 +1,36 @@
-from math import pi
+from scipy.constants import pi, day, value, kilo
+R = value('molar gas constant')
 
-#Physical constants
-c = 3.e8 # Speed of light in m/s
-free_space_imp = 120*pi # Impedence of free space
-k = 1.38e-23 # Boltzmann's constant in J/K
-density_water = 1e3 # Density of liquid water in kg m^-3
+#Earth
+Re = earth_avg_radious = 6.37e6 # m
+g = earth_avg_gravity = 9.81 # m s^-2
+omega = earth_avg_angular_vel = 2 * pi / day # rad s^-1
+d = earth_sfc_avg_dist_sun = 1.50e11 # m
+S = earth_solar_irradiance = 1.38e3 # W m^-2
 
-#Mass conversions
-g_per_kg = 1000.
+#Water
+Mw = water_molecular_weight = 18.016 # g / mol
+Rv = water_gas_constant = R / Mw * kilo #J K^-1 kg^-1
+rho_l = density_water = 1e3 # Nominal density of liquid water at 0C in kg m^-3
+Cp_v = water_vapor_specific_heat_press = 1952. # J K^-1 kg^-1
+Cv_v = water_vapor_specific_heat_vol = 1463. # J K^-1 kg^-1
+Cp_l = water_specific_heat = 4218. # at 0C J K^-1 kg^-1
+Lv = water_latent_heat_vaporization = 2.5e6 #0C J kg^-1
+Lf = water_latent_heat_fustion = 3.34e5 #0C J kg^-1
+Cp_i = ice_specific_heat = 2106 # at 0C J K^-1 kg^-1
+rho_i = density_ice = 917 # at 0C in kg m^-3
 
-#Distance conversions
-cm_per_m = 100.
-mm_per_cm = 10.
-mm_per_m = mm_per_cm * cm_per_m
-m_per_km = 1000.
-cm_per_in = 2.54
-nmi_per_m = 1./1852.354944
+#Dry air
+Md = dry_air_molecular_weight = 28.97 # g / mol at the sfc
+Rd = dry_air_gas_constant = R / Md * kilo # J K^-1 kg^-1
+Cp_d = dry_air_spec_heat_press = 1004. # J K^-1 kg^-1
+Cv_d = dry_air_spec_heat_vol = 717. # J K^-1 kg^-1
+dry_air_density_stp = 1.275 # at 0C 1000mb in kg m^-3
 
-#Angular conversion
-deg_per_rad = 180./pi # Duh.
+#General meteorology constants
+P0 = pot_temp_ref_press = 100000. # Pa
+kappa = poisson_exponent = Rd / Cp_d
+gamma_d = dry_adiabatic_lapse_rate = g / Cp_d * kilo # K km^-1
+epsilon = molecular_weight_ratio = Mw / Md
 
-#Time conversions
-sec_per_min = 60.
-min_per_hour = 60.
-sec_per_hour = sec_per_min * min_per_hour
-hour_per_day = 24.
-sec_per_day = sec_per_hour * hour_per_day
-
-#Temperature conversions
-degf_per_degc = 9./5.
-kelvin_offset = 273.15
+del pi, day, R, value, kilo
