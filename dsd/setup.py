7932afa2d35ceec5619cb2459c72db19ef37f691
Ryan May <rmay31@gmail.com>
2008-10-03 21:30:13 +0000

    Add setup.py file for dsd.
    git-svn-id: svn+ssh://bragg-tun/home/rmay/src_repos/py_pkgs/trunk@55 30c3dafe-ad74-4c27-8e40-3f69a20387b2

---
+++ dsd/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+from dsd import __version__
+
+setup(name = 'dsd',
+      version = str(__version__),
+      py_modules = ['dsd'],
+      author = 'Ryan May',
+      author_email = 'rmay31@gmail.com',
+      platforms = ['Linux', 'UNIX', 'Windows', 'MacOSX'],
+      description = 'Functions for doing drop-size distribution calculations',
+      url = 'http://weather.ou.edu/~rmay/research.html',
+      )
+
