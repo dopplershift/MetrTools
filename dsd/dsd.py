fbae1e8fccdec52f2e7f806351fb32eef76ef029
Ryan May <rmay31@gmail.com>
2012-05-08 23:22:49 -0500

    More work to try to make it possible to use units when available, but not break it when not.

--- dsd/dsd.py
+++ dsd/dsd.py
@@ -11,19 +11,18 @@ from scipy.special import gamma as gamma_func
 __version__ = 0.7
 
 mp_N0 = 8.0e3 / milli # m^-3 mm^-1 / m mm^-1 -> m^-4
+gamma_power_scale = 1.0 # meters
 
-try:
-    import quantities as pq
-    mp_N0 = mp_N0 * pq.meter**-4
-    density_water = density_water * pq.kilogram / pq.meter**3
-except ImportError:
-    pass
+from .unit_helpers import check_units, update_consts
+update_consts(locals())
 
+@force_units(None, lam='meter^-1')
 def constrained_gamma_shape(lam):
     '''Calculates the shape factor (mu) for the constrained gamma relation
        as given by Zhang et al. (2001), using the slope factor *lam*.'''
     return -0.016 * lam**2 + 1.213 * lam - 1.957
 
+@check_units(lwc='kg/m^3')
 def mp_slope_3rd(lwc):
     '''Gives the slope for the Marshall-Palmer drop size distribution
        given the liquid water content density in kg/m^3. Returns
@@ -36,38 +35,44 @@ def mp_from_lwc(d, lwc):
        slope parameter.  All quantities should be in MKS.'''
     return marshall_palmer(d, mp_slope_3rd(lwc))
 
+@check_units(d='meters', lam='1/meter')
 def marshall_palmer(d, lam):
     '''Returns the Marshall-Palmer distribution weights corresponding to the
        given diameters using the given slope parameter.  All quantities should
        be in MKS.'''
     return exponential(d, lam, mp_N0)
 
+@check_units(d='meters', lam='1/meter', N0='meter^-4')
 def exponential(d, lam, N0):
     '''Returns the exponential distribution weights corresponding to the
        given diameters using the given slope and intercept parameters.  All
        quantities should be in MKS.'''
     return modified_gamma(d, lam, N0, 0.0)
 
+@check_units(d='meters', lam='1/meter', N0='meter^-4')
 def modified_gamma(d, lam, N0, mu):
     '''Returns the modifed gamma distribution weights corresponding to the
        given diameters using the given slope, intercept, and shape parameters.
        All quantities should be in MKS.'''
-    return N0 * d**mu * np.exp(-d * lam)
+    return N0 * (d / gamma_power_scale)**mu * exp(-d * lam)
 
+@check_units(d='meters', d0='meter', N='meter^-3')
 def volume_gamma(d, d0, N, nu=-0.8):
     '''Returns the gamma distribution weights corresponding to the
        given diameters using the given parameters.
        All quantities should be in MKS.'''
     rat = d / d0
     return ((N * (3 *(nu + 1) ** (nu + 1)) / (d0 * gamma_func(nu + 1)))
-        * (rat**(3*nu + 2) * np.exp(-(nu+1) * rat**3)))
+        * (rat**(3*nu + 2) * exp(-(nu+1) * rat**3)))
 
+@check_units(lwc='kg/m^3', N='m^-3')
 def gamma_d0_from_lwc(lwc, N):
     '''Returns the gamma distribution d0 parameter given the value of liquid
        water content and number concentration.
        All quantities should be in MKS.'''
     return np.power(6 * lwc / (np.pi * density_water * N), 1. / 3.)
 
+@force_units('meters/second', d='meters')
 def rain_fallspeed(d):
     '''Returns the raindrop fallspeed in m s^-1 given the drop diameter in m'''
     d = d / milli # Formulas need diameter in mm.  Assumed passed in as m.
@@ -77,6 +82,7 @@ def rain_fallspeed(d):
     vt = -0.1021 + d*(4.932 + d*(-0.9551 + d*(0.07934 - 0.002362*d)))
     return vt
 
+@check_units(d='meters', dsd='meters^-4')
 def lwc(d, dsd):
     '''Returns the liquid water content in kg/m^3 given the number density
     and diameters. These should be in MKS.'''
