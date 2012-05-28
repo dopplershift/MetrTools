5079f87d00f62afee42cd90cf831d4adf601ae29
Ryan May <rmay31@gmail.com>
2012-05-27 23:14:15 -0500

    Add helper to obtain a constrained gamma distribution from the moments.

--- dsd/dsd/__init__.py
+++ dsd/dsd/__init__.py
@@ -103,17 +103,25 @@ def rainrate(d, dsd, fallspeed):
     return (np.pi / 6.) *  np.trapz(d**3 * fallspeed * dsd, x=d, axis=0)
 
 @force_units('mm^-1', qr='kg/m^3', N='mm^-3')
-def constrained_gamma_slope(qr, N):
+def constrained_gamma_slope(qr, N, which=np.max):
     ratio = (6 / (np.pi * density_water.magnitude)) * (qr / N)
     print qr, N, ratio
     roots = np.roots([-4.096e-6, 0.000931584, -0.070592688,
         1.779763333 - ratio, 0.205717849, -1.206271489, -0.042920493])
-    print roots
-    return roots[(roots.real>0) & (roots.imag == 0.)].max().real
+    slopes = roots[(roots.real>0) & (roots.imag == 0.)]
+    print slopes
+    return which(slopes).real
 
 @check_units(N='meters^-3', slope='meter^-1', shape='dimensionless')
 def constrained_gamma_intercept(N, slope, shape):
     return N * slope ** (shape + 1) / gamma_func(shape + 1)
+
+@check_units(N='meters^3', qr='kg/m^3', d='mm')
+def constrained_gamma_from_moments(N, qr, d, which=np.max):
+    slope = constrained_gamma_slope(qr, N, which)
+    shape = constrained_gamma_shape(slope)
+    intercept = constrained_gamma_intercept(N, slope, shape)
+    return modified_gamma(d, lam, intercept, shape)
 
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
